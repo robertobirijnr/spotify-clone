7af9901e014a9451316b92d059e11aa70538b419
robertobirijnr <robertobirijnr@hotmail.com>
2020-08-02 22:39:09 +0000

    top nav bar

--- src/components/sideNav.vue
+++ src/components/sideNav.vue
@@ -1,79 +1,79 @@
 <template>
-   <div class="flex" style="height:90vh">
-      <div class="w-56 bg-black h-full flex-none">
-        <div class="p-6">
-          <img src="../assets/logo-01.png" alt="" />
-        </div>
-        <div class="mx-2 mb-5">
-          <button
-            v-for="page in pages"
-            :key="page.id"
-            @click="setID = page.id"
-            :class="
-              `w-full text-sm font-semibold rounded text-white px-3 py-2 flex items-cnter justify-start ${
-                setID === page.id ? 'bg-teal-300 text-white' : 'bg-yellow'
-              }`
-            "
+  <div class="flex" style="height:90vh">
+    <div class="w-56 bg-black h-full flex-none">
+      <div class="p-6">
+        <img src="../assets/logo-01.png" alt="" />
+      </div>
+      <div class="mx-2 mb-5">
+        <button
+          v-for="page in pages"
+          :key="page.id"
+          @click="setID = page.id"
+          :class="
+            `w-full text-sm font-semibold focus:outline-none rounded text-white px-3 py-2 flex items-cnter justify-start ${
+              setID === page.id ? 'bg-teal-300 text-white' : 'bg-yellow'
+            }`
+          "
+        >
+          <i class="material-icons mr-3">{{ page.icon }}</i>
+          <p>{{ page.name }}</p>
+        </button>
+      </div>
+      <div class="mx-5">
+        <h1 class="text-xs mb-3 text-white tracking-widest uppercase">
+          Playlists
+        </h1>
+        <button
+          class="flex text-white justify-start item-center opacity-75 hover:opacity-100 mb-2"
+        >
+          <div></div>
+          <i class="material-icons mr-3">create_new_folder</i>
+          <p class="text-white text-sm font-semibold">Create Playlist</p>
+        </button>
+        <button
+          class="flex text-white justify-start item-center opacity-75 hover:opacity-100"
+        >
+          <div></div>
+          <i class="material-icons mr-3">stars</i>
+          <p class="text-white text-sm font-semibold">Liked Songs</p>
+        </button>
+        <div class="h-px w-full bg-white my-3"></div>
+      </div>
+      <div class="mx-5 pb-3">
+        <div class="w-full h-24 overflow-y-scroll">
+          <p
+            v-for="album in albums"
+            :key="album.name"
+            class="text-white hover:text-teal text-sm py-1"
           >
-            <i class="material-icons mr-3">{{ page.icon }}</i>
-            <p>{{ page.name }}</p>
-          </button>
+            {{ album.name }}
+          </p>
         </div>
-        <div class="mx-5">
-          <h1 class="text-xs mb-3 text-white tracking-widest uppercase">
-            Playlists
-          </h1>
-          <button
-            class="flex text-white justify-start item-center opacity-75 hover:opacity-100 mb-2"
-          >
-            <div></div>
-            <i class="material-icons mr-3">create_new_folder</i>
-            <p class="text-white text-sm font-semibold">Create Playlist</p>
-          </button>
-          <button
-            class="flex text-white justify-start item-center opacity-75 hover:opacity-100"
-          >
-            <div></div>
-            <i class="material-icons mr-3">stars</i>
-            <p class="text-white text-sm font-semibold">Liked Songs</p>
-          </button>
-          <div class="h-px w-full bg-white my-3"></div>
-        </div>
-        <div class="mx-5 pb-3">
-          <div class="w-full h-24 overflow-y-scroll">
-            <p
-              v-for="album in albums"
-              :key="album.name"
-              class="text-white hover:text-teal text-sm py-1"
-            >
-              {{ album.name }}
-            </p>
-          </div>
-          
-        </div>
-        <div class="mx-5 pb-3">
-         <button
-            class="flex text-white justify-start item-center opacity-75 hover:opacity-100"
-          >
-            <div></div>
-            <i class="material-icons mr-3">cloud_download</i>
-            <p class="text-white text-sm font-semibold">Install App</p>
-          </button>
-       
+      </div>
+      <div class="mx-5 pb-3">
+        <button
+          class="flex text-white justify-start item-center opacity-75 hover:opacity-100"
+        >
+          <div></div>
+          <i class="material-icons mr-3">cloud_download</i>
+          <p class="text-white text-sm font-semibold">Install App</p>
+        </button>
       </div>
       <div class="relative">
-        <img src="../assets/Kwesi-Arthur-.jpg" height="10px" alt="">
-      </div>
-      </div>
-      <div class="w-full h-full">
-        <div class="w-full sticky top-0 p-2"></div>
+        <img src="../assets/Kwesi-Arthur-album.jpg" height="10px" alt="" />
       </div>
     </div>
+    <TopNav />
+  </div>
 </template>
 
 <script>
-    export default {
-        data() {
+import TopNav from "@/components/topNav.vue";
+export default {
+  components: {
+    TopNav,
+  },
+  data() {
     return {
       pages: [
         { id: "home", name: "Home", icon: "home" },
@@ -91,9 +91,7 @@
       ],
     };
   },
-    }
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
---
+++ src/components/topNav.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="w-full h-full relative">
+    <div class="w-full sticky top-0 py-4 px-6 flex item-center justify-between">
+      <div class="flex item-center">
+        <button class="rounded-full bg-black w-8 h8 text-white mr-3">
+          <i class="material-icons">navigate_before</i>
+        </button>
+        <button class="rounded-full bg-black w-8 h8 text-white">
+          <i class="material-icons">navigate_next</i>
+        </button>
+      </div>
+      <div class="relative">
+        <button
+          @click="showdropdown = true"
+          class="focus:outline-none bg-black rounded-full py-1 px-2 flex items-center"
+        >
+          <p class="text-white font-semibold text-xs mr-3">Robert Obiri Jnr</p>
+          <i v-if="showdropdown === false" class="material-icons text-white"
+            >arrow_drop_down</i
+          >
+          <i v-if="showdropdown === true" class="material-icons text-white"
+            >arrow_drop_up</i
+          >
+        </button>
+        <div
+          v-if="showdropdown === true"
+          class="absolute bg-gray-800 w-full rounded mt-1"
+        >
+          <button
+            @click="showdropdown = false"
+            class="focus:outline-none w-full text-sm py-2 text-gray-600 hover:text-white border-b border-white opacity-75 hover:opacity-100"
+          >
+            Register
+          </button>
+          <button
+            @click="showdropdown = false"
+            class="focus:outline-none w-full text-sm py-2 text-gray-600 hover:text-white"
+          >
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showdropdown: false,
+    };
+  },
+};
+</script>
